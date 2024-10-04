2aaef1a6ab73e670311650e6eb30359bf26a6457
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-04 09:01:24 -0300

    Finished header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import "./components/base.css";
 
 function App() {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,52 @@
+import { Link, useNavigate } from "react-router-dom";
+import "./header.css";
+import Dropdown from "react-bootstrap/Dropdown";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("usuario");
+    sessionStorage.removeItem("senha");
+    navigate("/login"); // Redireciona para a página de login
+  };
+
+  const isLoggedIn = sessionStorage.getItem("usuario") !== null;
+
+  return (
+    <nav>
+      <div id="dropdown-basic" style={{ backgroundColor: 'transparent' }}></div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Produtos</Link>
+        </li>
+        <div id="logo">
+          <h1>C-FREE</h1>
+        </div>
+        <li>
+          <Link to="/about">Sobre</Link>
+        </li>
+        <li>
+          <Link to="/login">Loja</Link>
+        </li>
+      </ul>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Conta
+        </Dropdown.Toggle>
+        <Dropdown.Menu id="dropdown-menu">
+          <Dropdown.Item href="#/action-1">Login</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Cadastrar produto</Dropdown.Item>
+          {isLoggedIn && (
+            <Dropdown.Item href="#/action-3">Logout</Dropdown.Item>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+    </nav>
+  );
+};
+
+export default Header;
